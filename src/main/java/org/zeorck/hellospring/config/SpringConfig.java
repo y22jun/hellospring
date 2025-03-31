9f158837bedcce1de9a511d3bdbf271c43b73255
y22jun <tlsdpwns0612@naver.com>
2025-04-01 00:26:24 +0900

    Feat: Jdbc 구현

--- src/main/java/org/zeorck/hellospring/config/SpringConfig.java
+++ src/main/java/org/zeorck/hellospring/config/SpringConfig.java
@@ -1,13 +1,24 @@
 package org.zeorck.hellospring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.zeorck.hellospring.repository.JdbcMemberRepository;
 import org.zeorck.hellospring.repository.MemberRepository;
 import org.zeorck.hellospring.repository.MemoryMemberRepository;
 import org.zeorck.hellospring.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
