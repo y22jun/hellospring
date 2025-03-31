ac565d314640ab313432b4d40749ddd9448b897b
y22jun <tlsdpwns0612@naver.com>
2025-03-31 18:19:12 +0900

    Feat: Member domain, MemberRepository 설게

---
+++ src/main/java/org/zeorck/hellospring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package org.zeorck.hellospring.repository;
+
+import org.zeorck.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
